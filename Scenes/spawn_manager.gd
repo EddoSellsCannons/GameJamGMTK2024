7668a5edef4526e751ab569ac9c5e68039143d22
EddoSellsCannons <edmondhuang68@gmail.com>
2024-08-17 11:59:17 +1000

    Added Large and med microbe enemy Modified spawn to randomly pick from enemy list

--- Scenes/spawn_manager.gd
+++ Scenes/spawn_manager.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 @onready var S_microbe = preload("res://Scenes/s_microbe.tscn")
+@onready var M_microbe = preload("res://Scenes/m_microbe.tscn")
+@onready var L_microbe = preload("res://Scenes/l_microbe.tscn")
+
+@onready var enemyList = [S_microbe, M_microbe, L_microbe]
+
 @onready var gameManager = $".."
 
 func _on_spawn_timer_timeout() -> void:
-	var enemyToAdd = S_microbe.instantiate()
+	spawnEnemy()
+
+func spawnEnemy():
+	var enemyToAdd = enemyList.pick_random().instantiate()
 	var posToSpawn = get_tree().get_nodes_in_group("spawnpoint").pick_random().position
 	enemyToAdd.position = posToSpawn
 	gameManager.add_child(enemyToAdd)
