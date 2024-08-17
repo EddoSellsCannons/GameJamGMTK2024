814d67d468113d80cb9f72daaf14e510ed96ead4
EddoSellsCannons <edmondhuang68@gmail.com>
2024-08-17 19:38:18 +1000

    Added S_animal Added player proj added tree & proj

---
+++ Scenes/frog_player.gd
@@ -0,0 +1,96 @@
+extends CharacterBody2D
+
+const STAMINA_CAP = 200.0
+const SHOOT_COST = 50.0
+
+var speed = 80.0
+var size = 100.0
+const sizeConsumeMultiplier = 0.05
+
+var dashMultiplier = 3
+
+@onready var anim_player = $AnimationPlayer
+@onready var invulTimer = $invulTimer
+@onready var gameManager = $".."
+
+var isInvul: bool
+
+var moveDir
+
+var stamina =  STAMINA_CAP
+
+@onready var playerProj = preload("res://Scenes/player_proj.tscn")
+
+func _ready() -> void:
+	updateSizing()
+	speed = (size/80 * 100)
+
+func _physics_process(delta: float) -> void:
+	handleInput()
+	move_and_slide()
+	regenStamina()
+
+func handleInput():
+	moveDir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDir * speed
+	if moveDir.x > 0:
+		$Sprite2D.flip_h = false
+	else:
+		$Sprite2D.flip_h = true
+	if Input.is_action_pressed("ui_accept"):
+		dash()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		if sizeCompare(area.size):
+			size += area.maxSize * sizeConsumeMultiplier
+			speed = (size/80 * 100) #80% of size
+			area.queue_free()
+			updateSizing()
+			anim_player.play("eatSomething")
+
+func sizeCompare(enemySize):
+	if size > (enemySize/80 * 100):
+		return true
+	else:
+		return false
+
+func updateSizing():
+	scale = Vector2(size/100, size/100)
+	#$Camera2D.zoom = Vector2(1 - size/1000, 1 - size/1000)
+
+func tookDamage():
+	if isInvul:
+		return
+	if size > 100:
+		size *= 0.9
+	updateSizing()
+	anim_player.play("damageTaken")
+	isInvul = true
+	invulTimer.start()
+	await invulTimer.timeout
+	anim_player.play("RESET")
+	isInvul = false
+
+func dash():
+	if stamina > 0:
+		velocity += moveDir * speed * dashMultiplier
+		stamina -= 1
+		
+func regenStamina():
+	if stamina <= STAMINA_CAP:
+		stamina += 0.6
+
+func shootProj(dir):
+	if stamina > 0:
+		stamina -= SHOOT_COST
+		var proj = playerProj.instantiate()
+		proj.position = position
+		proj.dir = dir
+		gameManager.add_child(proj)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		var mouse_position = get_global_mouse_position()
+		var projDir = (mouse_position - global_position).normalized()
+		shootProj(projDir)
---
+++ Scenes/spawn_manager_lvl3.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+
+@onready var S_fish = preload("res://Scenes/s_animal.tscn")
+@onready var M_fish = preload("res://Scenes/m_fish.tscn")
+@onready var L_fish = preload("res://Scenes/l_fish.tscn")
+
+@onready var treeObstacle = preload("res://Scenes/treeObstacle.tscn")
+
+@onready var enemyList = [S_fish, M_fish, L_fish]
+
+@onready var gameManager = $".."
+
+func _ready() -> void:
+	await get_tree().create_timer(1).timeout 
+	spawnObstacles()
+	for i in range(50):
+		#await get_tree().create_timer(0.1).timeout 
+		spawnEnemy()
+
+func _on_spawn_timer_timeout() -> void:
+	spawnEnemy()
+
+func spawnEnemy():
+	var fishTypeToSpawn = 5#rng.randi_range(0, 100)
+	var fishIndex
+	if fishTypeToSpawn >= 0 and fishTypeToSpawn <= 70:
+		fishIndex = 0
+	elif fishTypeToSpawn >= 71 and fishTypeToSpawn <= 94:
+		fishIndex = 1
+	elif fishTypeToSpawn >= 95 and fishTypeToSpawn <= 100:
+		fishIndex = 2
+	var enemyToAdd = enemyList[fishIndex].instantiate()
+	var posToSpawn = get_tree().get_nodes_in_group("spawnpoint").pick_random().position
+	enemyToAdd.position = posToSpawn
+	gameManager.add_child(enemyToAdd)
+
+func spawnObstacles():
+	for i in range(50):
+		var coords = Vector2(rng.randf_range(-5000, 5000), rng.randf_range(-5000, 5000))
+		var t = treeObstacle.instantiate()
+		t.position = coords
+		gameManager.add_child(t)
