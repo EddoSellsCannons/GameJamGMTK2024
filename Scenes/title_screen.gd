134aa4d6d255995c1450ff071c378108c8063f48
EddoSellsCannons <edmondhuang68@gmail.com>
2024-08-18 18:06:07 +1000

    adedd music to title screen

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-
 func _on_start_button_button_down() -> void:
-	$AnimationPlayer.play("levelTransition")
+	$microbe/AnimationPlayer.play("levelTransition")
 
 func start_game():
 	get_tree().change_scene_to_file("res://Scenes/game_manager.tscn")
